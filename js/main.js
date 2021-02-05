2a6bbea782d680be2c52850bcb1eba1c22ec197b
Bekbab <72729623+Bekbab@users.noreply.github.com>
2021-02-05 09:33:20 +0100

    Generell idé klar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var idag = new Date();
+var dd = String(today.getDate()).padStart(2, '0'); //.padstart lägger till nollor tills månadsformatet dd 
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+document.write(today);
